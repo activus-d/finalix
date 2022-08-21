f53d82a79a1bc90b97695ecf70a390f9c494fd18
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-08-21 18:30:34 +0100

    navbar

--- components/HomeFinalix.jsx
+++ components/HomeFinalix.jsx
@@ -7,47 +7,37 @@ const HomeFinalix = () => {
     <section className='flex relative   justify-between'>
       <div className=' py-28 sm:bg px-6  '>
         <div className=' h-full w-full  ss:w-5/6 sm:w-4/5  mx-auto '>
-        <h1 className='font-bold capitalize text-secHead text-priHeadColor'>
+          <h1 className='font-bold capitalize text-secHead text-priHeadColor'>
           Finalix business consulting
-        </h1>
-        <p className='sm:w-3/4 text-homeTextColor py-8 text-priPara'>Finalix develops and supports projects for companies in the finance and insurance industry. Our clients include universal banks, private banks, asset managers, private equity companies and insurance companies.
-        </p>
+          </h1>
+          <p className='sm:w-3/4 text-homeTextColor py-8 text-priPara'>Finalix develops and supports projects for companies in the finance and insurance industry. Our clients include universal banks, private banks, asset managers, private equity companies and insurance companies.
+          </p>
 
-        <div className='py-10'>
-          <h2 className='text-secHead text-priHeadColor font-bold'>Expertise</h2>
-          <p className='md:w-[45rem] text-homeTextColor sm:w-2/3  w-full py-8 text-priPara'>You benefit from experienced and conceptually strong consultants who develop solutions to strengthen your company on the market in the long term.
-
-        </p>
-        </div>
+          <div className='py-10'>
+            <h2 className='text-secHead text-priHeadColor font-bold'>Expertise</h2>
+            <p className='md:w-[45rem] text-homeTextColor sm:w-2/3  w-full py-8 text-priPara'>You benefit from experienced and conceptually strong consultants who develop solutions to strengthen your company on the market in the long term.
+            </p>
+          </div>
+        </div>  
       </div>
-      
-    </div>
-    <div className='hidden sm:flex flex-col  items-center  relative w-[50rem]'>
-      
-        <div className='one w-[28rem] md:w-[34rem] h-[9rem] '>
-        </div>
+      <div className='hidden sm:flex flex-col  items-center  relative w-[50rem]'>
+        <div className='one w-[28rem] md:w-[34rem] h-[9rem] '></div>
         <div className=' absolute right-8 top-32 '>
-        <Image 
-        src={rec1}
-        width={220}
-        height={180}
-        className='img'
-      />
-      </div>
-        
-      
-     <div className='two  w-[35rem] md:w-[43rem] h-[12rem]'>
+          <Image 
+            src={rec1}
+            width={220}
+            height={180}
+          />
         </div>
+        
+        <div className='two  w-[35rem] md:w-[43rem] h-[12rem]'></div>
         <div className='absolute sm:right-16 lg:right-40  md:bottom-[-15px] sm:bottom-8'>
-        <Image 
-        src={rec4}
-        width={250}
-        height={240}
-        className='img'
-      />
-      </div>
-
-      
+          <Image 
+            src={rec4}
+            width={250}
+            height={240}
+          />
+        </div>
       </div>
     </section>
     
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,17 +6,16 @@ export const Menu =()=>{
 return(
   <ul className='block w-fit ss:flex gap-12 cursor-pointer '>
         <a href='/'>
-        <li className='hover:border-b-2 pb-2'>Home</li>
+          <li className='hover:border-b-2 pb-2'>Home</li>
         </a>
           <Link href='/Services'>
         <li className=' border-b-2 hover:border-b-2 '>Services</li>
         </Link>
-        
         <Link href='/Experts'>
-        <li className='hover:border-b-2 pt-2'>Experts</li>
+          <li className='hover:border-b-2 pt-2'>Experts</li>
         </Link>
         <Link href='/Contact'>
-        <li className='hover:border-b-2 pt-2'>Contact</li>
+          <li className='hover:border-b-2 pt-2'>Contact</li>
         </Link>
       </ul>
 )
@@ -27,27 +26,21 @@ const Navbar = () => {
   return (
     <nav className='ss:flex py-7 justify-between items-center text-secPara text-secColor font-bold  w-full px-6 ss:px-0 ss:w-5/6 mx-auto'>
       <div className='flex justify-between items-center'>
-
-      
-        <h3 className='text-mediumHead'><a href="/">Finalix</a> </h3>
+        <h3 className='text-mediumHead'>
+          <a href="/">Finalix</a> 
+        </h3>
         <div className='ss:hidden block '>
           {menuToggle 
             ? <AiOutlineClose color='#08352A' size={27} onClick={()=> setMenuToggle(false)}/>
             : <AiOutlineMenu color='#08352A' size={27} onClick={()=> setMenuToggle(true)}/>
           }
         </div>
-      
-     
-      
-
-       
-
       </div>
-       <div className='hidden ss:block'>
+      <div className='hidden ss:block'>
         <Menu/>
       </div>
       {menuToggle &&
-         <div className='ss:hidden  pr-12  py-4 min-w-[250px]'>
+        <div className='ss:hidden  pr-12  py-4 min-w-[250px]'>
   
         <Menu/>
       </div>
